4eec48d5b6be894d86d432bf2864188fc52af2da
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-07-17 20:14:28 +0530

    sorting is emplemented

--- src/Components/ActionPage.jsx
+++ src/Components/ActionPage.jsx
@@ -1,51 +1,94 @@
-import logo from '../Assests/Images/logo.png'
-import profile from '../Assests/Images/profile.png'
-import mic from '../Assests/Images/mic.png'
-import search from '../Assests/Images/search.png'
-import hamburger from '../Assests/Images/hamburger.png'
-import Details from "../Components/Details"
+import  { useState, useEffect } from 'react';
+import logo from '../Assests/Images/logo.png';
+import profile from '../Assests/Images/profile.png';
+import mic from '../Assests/Images/mic.png';
+import search from '../Assests/Images/search.png';
+import hamburger from '../Assests/Images/hamburger.png';
+import Details from '../Components/Details';
+import Data from './Data.json';
 
+export default function ActionPage() {
+  const [sortedData, setSortedData] = useState(Data);
+  const [sortingOption, setSortingOption] = useState('');
 
+  useEffect(() => {
+    const sortData = () => {
+      let sorted = [...Data];
 
+      if (sortingOption === 'lowToHigh') {
+        sorted.sort((a, b) => a.Prices - b.Prices);
+      } else if (sortingOption === 'highToLow') {
+        sorted.sort((a, b) => b.Prices - a.Prices);
+      }
 
-export default function ActionPage () {
+      setSortedData(sorted);
+    };
+
+    sortData();
+  }, [sortingOption]);
+
+  const handleSortingChange = (event) => {
+    setSortingOption(event.target.value);
+  };
+
   return (
-    <section className='Actionpage'>
-      <div className='ActionpageNavbar'>
-        <div className='Actionpagelogo'>
-          <img src={logo} alt='logo' />
+    <section className="Actionpage">
+      <div className="ActionpageNavbar">
+        <div className="Actionpagelogo">
+          <img src={logo} alt="logo" />
         </div>
-        <div className='ActionPage-input'>
-          <div className='selection'>
-            <select name='input' id='selectfield'>
-              <option value='Rent'>Rent</option>
-              <option value='Buy'>Buy</option>
+        <div className="ActionPage-input">
+          <div className="selection">
+            <select name="input" id="selectfield">
+              <option value="Rent">Rent</option>
+              <option value="Buy">Buy</option>
             </select>
           </div>
 
-          <div className='inputdiv'>
-            <input type='text' placeholder='Search Place' className='inputfield'/>
+          <div className="inputdiv">
+            <input type="text" placeholder="Search Place" className="inputfield" />
           </div>
-          <div className='micicons'>
-            <img src={mic} alt='mic' />
+          <div className="micicons">
+            <img src={mic} alt="mic" />
           </div>
-          <div className='inputicon'>
-            <img src={search} alt='searchicon' />
+          <div className="inputicon">
+            <img src={search} alt="searchicon" />
           </div>
         </div>
-        <div className='contactprofilesection'>
-          <div className='buttondiv'>
-            <button className='button'>Post property</button>
+        <div className="contactprofilesection">
+          <div className="buttondiv">
+            <button className="button">Post property</button>
           </div>
-          <div className='profileicons' >
-            <img src={profile} alt='Profile images' />
+          <div className="profileicons">
+            <img src={profile} alt="Profile images" />
           </div>
           <div>
-            <img src={hamburger} alt='' />
+            <img src={hamburger} alt="" />
           </div>
         </div>
       </div>
-      <Details/>
+      <div>
+        <div className="sorting">
+          <select name="input" id="selectfield" onChange={handleSortingChange} className="sort">
+            <option value="">Sort</option>
+            <option value="lowToHigh">Low To High</option>
+            <option value="highToLow">High To Low</option>
+          </select>
+        </div>
+      </div>
+      {sortedData.map((data, index) => (
+        <Details
+          key={index}
+          title={data.title}
+          image={data.image}
+          locations={data.locations}
+          Prices={data.Prices}
+          amenities={data.amenities}
+          area={data.area}
+          type={data.type}
+          summary={data.summary}
+        />
+      ))}
     </section>
-  )
+  );
 }
--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,57 +1,52 @@
-export default function Details () {
+/* eslint-disable react/prop-types */
+
+
+export default function Details(props) {
   return (
     <section className='detailpage'>
-      <div className='leftside'>leftpage</div>
+      <div className='leftside'>leftside</div>
       <div className='productdetailsection'>
         <div className='homesimage'>
-          <img
-            src='https://mediacdn.99acres.com/media1/21364/17/427297762O-1686310872399.jpg'
-            alt=''
-          />
+          <img src={props.image} alt='image' />
         </div>
         <div className='homedetail'>
           <div className='homeheader'>
             <div className='hometitle'>
-              <h2>H2 Bhk Independent Builder Floor in sector 65 gurgaon</h2>
+              <h2>{props.title}</h2>
             </div>
             <div className='placesnearby listitem'>
               <ul>
-                <li>47 places Nearby</li>
-                <li>47 places Nearby</li>
-                <li>47 places Nearby</li>
-                <li>47 places Nearby</li>
+                <li>{props.locations[0]}</li>
+                <li>{props.locations[1]}</li>
+                <li>{props.locations[2]}</li>
+                <li>{props.locations[3]}</li>
               </ul>
             </div>
             <div className='homeprices listitem'>
               <ul>
-                <li>3.7 Cr </li>
-                <li>3600sq.ft </li>
-                <li>4 BHK </li>
+                <li>{props.Prices}</li>
+                <li>{props.area}</li>
+                <li>{props.type}</li>
               </ul>
             </div>
             <div className='facingdetails listitem'>
               <ul>
-                <li>Northfacing</li>
-                <li>Northfacing</li>
-                <li>Northfacing</li>
-                <li>Northfacing</li>
+                <li>{props.amenities[0]}</li>
+                <li>{props.amenities[1]}</li>
+                <li>{props.amenities[2]}</li>
+                <li>{props.amenities[3]}</li>
               </ul>
             </div>
             <div>
-            <h4 className="summary">
-              4 bhk with attached bathroom Powder room, new contraction italian
-            </h4>
+              <h4 className="summary">{props.summary}</h4>
             </div>
-           
             <div className='phonenumber'>
-               <h2>View Phone Number</h2>
-            <button>Contact Dealer</button>
+              <h2>View Phone Number</h2>
+              <button>Contact Dealer</button>
+            </div>
           </div>
-          </div>
-
-         
         </div>
       </div>
     </section>
-  )
+  );
 }
